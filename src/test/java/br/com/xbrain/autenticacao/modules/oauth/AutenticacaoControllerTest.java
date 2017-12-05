3f0f11faaac1292fdf2e8f36261d15aec0870ae3
rafaelzeffa <rafael@xbrain.com.br>
2017-12-05 10:42:03 -0200

    Adiciona testes para a checagem de token

--- src/test/java/br/com/xbrain/autenticacao/modules/oauth/AutenticacaoControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/oauth/AutenticacaoControllerTest.java
@@ -13,7 +13,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
@@ -41,5 +45,27 @@ public class AutenticacaoControllerTest {
     public void deveNaoAutenticar() {
         OAuthToken token = TestsHelper.getAccessTokenObject(mvc, "INVALIDO@XBRAIN.COM.BR");
         assertNull(token.getAccessToken());
+    }
+
+    @Test
+    public void deveChecarATokenComoValida() throws Exception {
+        OAuthToken token = TestsHelper.getAccessTokenObject(mvc, Usuarios.ADMIN);
+
+        mvc.perform(
+                post("/oauth/check_token")
+                        .param("token", token.getAccessToken()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nome", is("ADMIN")))
+                .andExpect(jsonPath("$.login", is("200-ADMIN@XBRAIN.COM.BR")))
+                .andExpect(jsonPath("$.departamento", is("Administrativo")))
+                .andExpect(jsonPath("$.cargo", is("ADMINISTRADOR")));
+    }
+
+    @Test
+    public void deveChecarATokenComoInvalida() throws Exception {
+        mvc.perform(
+                post("/oauth/check_token")
+                        .param("token", "teste"))
+                .andExpect(status().isBadRequest());
     }
 }
