afbf678de2d62ef685d01a7aee0f62c58844090d
rafaelzeffa <rafael@xbrain.com.br>
2017-12-05 16:45:25 -0200

    Altera config de https para nao redirecionar
    A config antiga redirecionava os requests de http para https

--- src/main/java/br/com/xbrain/autenticacao/config/HttpsConfig.java
+++ src/main/java/br/com/xbrain/autenticacao/config/HttpsConfig.java
@@ -1,9 +1,6 @@
 package br.com.xbrain.autenticacao.config;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -13,36 +10,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HttpsConfig {
 
-    @Value("${server.port}")
-    private Integer httpsPort;
-
     @Value("${server.http-port}")
     private Integer httpPort;
 
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-
-        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
         return tomcat;
     }
 
-    private Connector initiateHttpConnector() {
+    private Connector createStandardConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
         connector.setPort(httpPort);
-        connector.setSecure(false);
-        connector.setRedirectPort(httpsPort);
         return connector;
     }
 }
