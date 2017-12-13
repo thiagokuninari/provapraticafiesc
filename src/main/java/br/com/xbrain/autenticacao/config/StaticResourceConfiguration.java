d4600f27e9a29ae54a6c0be16d9e08efa0518e99
rafaelzeffa <rafael@xbrain.com.br>
2017-12-13 11:48:12 -0200

    Adiciona config para os arquvos estaticos

---
+++ src/main/java/br/com/xbrain/autenticacao/config/StaticResourceConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.xbrain.autenticacao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceConfiguration {
+
+    @Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/login").setViewName("forward:/index.html");
+            }
+        };
+    }
+}
+
