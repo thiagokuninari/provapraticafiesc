7ebc4e27a40d52fb16cd9b7f89f7c072edd23ce3
andrebartieres <cbxy2812>
2018-03-29 09:21:30 -0300

    Corrige teste Motivo Inativação

--- src/test/java/br/com/xbrain/autenticacao/modules/usuario/controller/MotivoInativacaoControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/usuario/controller/MotivoInativacaoControllerTest.java
@@ -44,7 +44,7 @@ public class MotivoInativacaoControllerTest {
                 .header("Authorization", getAccessToken(mvc, ADMIN))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$", hasSize(4)))
                 .andExpect(jsonPath("$[0].descricao", is("FÉRIAS")));
     }
 
