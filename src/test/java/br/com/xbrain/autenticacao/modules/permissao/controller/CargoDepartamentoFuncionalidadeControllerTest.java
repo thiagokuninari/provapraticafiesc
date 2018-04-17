d0b7dfaaf7683e3a4f43a9f3aef43c18e89c8d68
andrebartieres <cbxy2812>
2018-04-17 11:00:18 -0300

    Cria tela para gerenciar Motivo de Telefonia

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
@@ -51,7 +51,7 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .header("Authorization", getAccessToken(mvc, ADMIN))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(200)));
+                .andExpect(jsonPath("$", hasSize(201)));
     }
 
     @Test
@@ -61,8 +61,8 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(10)))
-                .andExpect(jsonPath("$.totalPages", is(20)))
-                .andExpect(jsonPath("$.totalElements", is(200)));
+                .andExpect(jsonPath("$.totalPages", is(21)))
+                .andExpect(jsonPath("$.totalElements", is(201)));
     }
 
     @Test
