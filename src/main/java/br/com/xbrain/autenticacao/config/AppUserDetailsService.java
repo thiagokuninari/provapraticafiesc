9b776bf03f0e70b29e74c75fdabc8eeb8886b21d
andrebartieres <cbxy2812>
2018-05-14 10:30:28 -0300

    Permite emular usuário invativo

--- src/main/java/br/com/xbrain/autenticacao/config/AppUserDetailsService.java
+++ src/main/java/br/com/xbrain/autenticacao/config/AppUserDetailsService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class AppUserDetailsService implements UserDetailsService {git
+public class AppUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
