6c7d70e6e49c9a41fe2cd84f9395b3a472ea3447
Thiago Marcello <thiagomarcello@xbrain.com.br>
2018-05-15 17:05:05 -0300

    Adiciona nova permissão para tela de tabulação

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
@@ -51,7 +51,7 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .header("Authorization", getAccessToken(mvc, ADMIN))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(202)));
+                .andExpect(jsonPath("$", hasSize(206)));
     }
 
     @Test
@@ -62,7 +62,7 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(10)))
                 .andExpect(jsonPath("$.totalPages", is(21)))
-                .andExpect(jsonPath("$.totalElements", is(202)));
+                .andExpect(jsonPath("$.totalElements", is(206)));
     }
 
     @Test
