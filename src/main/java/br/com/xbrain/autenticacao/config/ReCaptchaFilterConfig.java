0ee4d92f389c13ff7c580d8ab7098c1d5eb06c5a
andrebartieres <cbxy2812>
2018-06-21 15:53:07 -0300

    Define reCaptcha só para Produção

--- src/main/java/br/com/xbrain/autenticacao/config/ReCaptchaFilterConfig.java
+++ src/main/java/br/com/xbrain/autenticacao/config/ReCaptchaFilterConfig.java
@@ -3,8 +3,10 @@ package br.com.xbrain.autenticacao.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("producao")
 public class ReCaptchaFilterConfig {
 
     @Bean
