c4af0203ce7d5258ec9a797e9775e82043b65ffd
andrebartieres <cbxy2812>
2018-07-04 17:00:32 -0300

    #10283 - Adiciona permissões de Dashboard

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
@@ -51,7 +51,7 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .header("Authorization", getAccessToken(mvc, ADMIN))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(229)));
+                .andExpect(jsonPath("$", hasSize(466)));
     }
 
     @Test
@@ -61,8 +61,8 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(10)))
-                .andExpect(jsonPath("$.totalPages", is(23)))
-                .andExpect(jsonPath("$.totalElements", is(229)));
+                .andExpect(jsonPath("$.totalPages", is(47)))
+                .andExpect(jsonPath("$.totalElements", is(466)));
     }
 
     @Test
