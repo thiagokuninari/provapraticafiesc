d04133f74d8f4f8db4cfdaa2f780a9dcb09c8d4a
Helton Hayashi <helton@xbrain.com.br>
2018-09-04 09:36:05 -0300

    #10519 Corrige testes

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
@@ -62,7 +62,7 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(10)))
                 .andExpect(jsonPath("$.totalPages", is(55)))
-                .andExpect(jsonPath("$.totalElements", is(540)));
+                .andExpect(jsonPath("$.totalElements", is(544)));
     }
 
     @Test
