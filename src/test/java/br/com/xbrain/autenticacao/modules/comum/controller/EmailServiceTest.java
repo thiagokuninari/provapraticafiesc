71ec17c998d5c9ae3cd126ae9b317fadbbb80071
Luis Gustavo Dias <luisdias@xbrain.com.br>
2018-09-06 15:26:25 -0300

    #10829 Altera sistema de envio de emails para o novo serviço

---
+++ src/test/java/br/com/xbrain/autenticacao/modules/comum/controller/EmailServiceTest.java
@@ -0,0 +1,104 @@
+package br.com.xbrain.autenticacao.modules.comum.controller;
+
+import br.com.xbrain.autenticacao.modules.email.service.EmailService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailServiceTest {
+
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private EmailService emailService;
+
+    private static final String ASSUNTO_EMAIL = "Teste de envio de Email";
+    private static final String CONTEUDO_EMAIL = "<html><head></head><body><h1>Olá mundo</h1></body></html>";
+
+    @Before
+    public void setup() {
+        when(restTemplate.postForEntity(anyString(), any(), any())).then(invocationOnMock -> null);
+    }
+
+    @Test
+    public void deveEnviarEmail() {
+        ReflectionTestUtils.setField(emailService, "enviarEmail", true);
+
+        emailService.enviarEmail(Collections.singletonList("luisdias@xbrain.com.br"),
+                ASSUNTO_EMAIL,
+                CONTEUDO_EMAIL,
+                "XBRAIN");
+
+        verify(restTemplate, times(1)).postForEntity(anyString(), any(), any());
+    }
+
+    @Test
+    public void deveEnviarEmailSomenteParaOsEmailsConfigurados() {
+        List<String> emails = Arrays.asList("teste@xbrain.com.br", "emailteste@xbrain.com.br");
+        ReflectionTestUtils.setField(emailService, "enviarEmail", true);
+        ReflectionTestUtils.setField(emailService, "emails", String.join(",", emails));
+
+        List<String> emailsParaEnviar = emailService.getEmails(Collections.singletonList("luisdias@xbrain.com.br"));
+        assertThat(emailsParaEnviar, hasItems("teste@xbrain.com.br", "emailteste@xbrain.com.br"));
+    }
+
+    @Test
+    public void naoDeveEnviarEmailSeEstiverDesativado() {
+        ReflectionTestUtils.setField(emailService, "enviarEmail", false);
+
+        emailService.enviarEmail(
+                Collections.singletonList("luisdias@xbrain.com.br"),
+                ASSUNTO_EMAIL,
+                CONTEUDO_EMAIL,
+                "XBRAIN");
+
+        verify(restTemplate, never()).postForEntity(anyString(), any(), any());
+    }
+
+    @Test
+    public void naoDeveEnviarEmailSemEmpresaAlias() {
+        emailService.enviarEmail(
+                Collections.singletonList("luisdias@xbrain.com.br"),
+                ASSUNTO_EMAIL,
+                CONTEUDO_EMAIL,
+                null);
+
+        verify(restTemplate, never()).postForEntity(anyString(), any(), any());
+    }
+
+    @Test
+    public void naoDeveEnviarEmailSemDestinatarios() {
+        emailService.enviarEmail(
+                new ArrayList<>(),
+                ASSUNTO_EMAIL,
+                CONTEUDO_EMAIL,
+                "XBRAIN");
+
+        verify(restTemplate, never()).postForEntity(anyString(), any(), any());
+    }
+}
