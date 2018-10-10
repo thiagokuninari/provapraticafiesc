c405bae91f297caab48cba058a4f087f222da3e5
Helton Hayashi <helton@xbrain.com.br>
2018-10-10 15:44:50 -0300

    Revert "Remove funcionalidade resetar senha"
    This reverts commit 96c050f919ab48ad4e0ecc3b0f0aaa5856e5f6c6.

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import br.com.xbrain.autenticacao.modules.usuario.dto.*;
 import br.com.xbrain.autenticacao.modules.usuario.enums.CodigoFuncionalidade;
 import br.com.xbrain.autenticacao.modules.usuario.enums.CodigoNivel;
 import br.com.xbrain.autenticacao.modules.usuario.service.UsuarioService;
+import br.com.xbrain.autenticacao.modules.usuario.service.UsuarioServiceEsqueciSenha;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +23,9 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private UsuarioServiceEsqueciSenha usuarioServiceEsqueciSenha;
 
     private Integer getUsuarioId(Principal principal) {
         return Integer.parseInt(principal.getName().split(Pattern.quote("-"))[0]);
@@ -132,14 +136,12 @@ public class UsuarioController {
     }
 
     @RequestMapping(value = "/esqueci-senha", method = RequestMethod.PUT)
-    public String esqueceuSenha(@RequestBody UsuarioDadosAcessoRequest dto) {
-        return "ok";
+    public void esqueceuSenha(@RequestBody UsuarioDadosAcessoRequest dto) {
         //usuarioServiceEsqueciSenha.enviarConfirmacaoResetarSenha(dto.getEmailAtual());
     }
 
     @GetMapping(value = "/resetar-senha")
-    public String resetarSenha(@RequestParam String token) {
-        return "ok";
+    public void resetarSenha(@RequestParam String token) {
         //usuarioServiceEsqueciSenha.resetarSenha(token);
     }
 }
