efd293b73c0548881f775a21dcceb9038a5132b8
rafaelzeffa <rafael@xbrain.com.br>
2018-10-26 17:04:41 -0200

    #11353 Adiciona url para deslogar todos os usuarios

--- src/main/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutController.java
@@ -1,6 +1,8 @@
 package br.com.xbrain.autenticacao.modules.autenticacao.controller;
 
 import br.com.xbrain.autenticacao.modules.autenticacao.service.AutenticacaoService;
+import br.com.xbrain.autenticacao.modules.comum.exception.PermissaoException;
+import br.com.xbrain.autenticacao.modules.usuario.enums.CodigoNivel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,14 @@ public class LogoutController {
     @RequestMapping(value = "/usuario/{usuarioId}", method = RequestMethod.GET)
     public void logout(@PathVariable("usuarioId") int usuarioId) {
         autenticacaoService.logout(usuarioId);
+    }
+
+    @RequestMapping(value = "/todos-usuarios", method = RequestMethod.GET)
+    public void logoutAll() {
+        if (autenticacaoService.getUsuarioAutenticado().getNivel().equals(CodigoNivel.XBRAIN.name())) {
+            autenticacaoService.logoutAllUsers();
+            return;
+        }
+        throw new PermissaoException();
     }
 }
--- src/test/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutControllerTest.java
@@ -1,11 +1,13 @@
 package br.com.xbrain.autenticacao.modules.autenticacao.controller;
 
+import br.com.xbrain.autenticacao.config.AuthServerConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static helpers.TestsHelper.getAccessToken;
 import static helpers.Usuarios.ADMIN;
+import static helpers.Usuarios.OPERACAO_GERENTE_COMERCIAL;
+import static org.junit.Assert.assertFalse;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +32,8 @@ public class LogoutControllerTest {
 
     @Autowired
     private MockMvc mvc;
+    @Autowired
+    private TokenStore tokenStore;
 
     @Test
     public void deveSolicitarAutenticacao() throws Exception  {
@@ -68,5 +74,26 @@ public class LogoutControllerTest {
         return mvc.perform(get("/api/empresas")
                 .header("Authorization", token)
                 .accept(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void devePermitirSomenteUsuariosXBrainFazerLogoutDeTodosOsUsuarios() throws Exception  {
+        mvc.perform(get("/api/logout/todos-usuarios")
+                .header("Authorization", getAccessToken(mvc, OPERACAO_GERENTE_COMERCIAL))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void deveDeslogarTodosOsUsuarios() throws Exception  {
+        getAccessToken(mvc, ADMIN);
+        getAccessToken(mvc, OPERACAO_GERENTE_COMERCIAL);
+
+        assertFalse(tokenStore.findTokensByClientId(AuthServerConfig.APP_CLIENT).isEmpty());
+
+        mvc.perform(get("/api/logout/todos-usuarios")
+                .header("Authorization", getAccessToken(mvc, ADMIN))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
