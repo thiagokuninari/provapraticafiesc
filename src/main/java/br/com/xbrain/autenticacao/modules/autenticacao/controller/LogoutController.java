4621d016da64358fe1cc3a4ab893e762183d7c4f
rafaelzeffa <rafael@xbrain.com.br>
2018-10-26 18:03:07 -0200

    #11353 Corrige mapeamento dos requests

--- src/main/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutController.java
@@ -4,9 +4,9 @@ import br.com.xbrain.autenticacao.modules.autenticacao.service.AutenticacaoServi
 import br.com.xbrain.autenticacao.modules.comum.exception.PermissaoException;
 import br.com.xbrain.autenticacao.modules.usuario.enums.CodigoNivel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,17 +16,17 @@ public class LogoutController {
     @Autowired
     private AutenticacaoService autenticacaoService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public void logout() {
         autenticacaoService.logout(autenticacaoService.getLoginUsuario());
     }
 
-    @RequestMapping(value = "/usuario/{usuarioId}", method = RequestMethod.GET)
+    @GetMapping("/usuario/{usuarioId}")
     public void logout(@PathVariable("usuarioId") int usuarioId) {
         autenticacaoService.logout(usuarioId);
     }
 
-    @RequestMapping(value = "/todos-usuarios", method = RequestMethod.GET)
+    @GetMapping("/todos-usuarios")
     public void logoutAll() {
         if (autenticacaoService.getUsuarioAutenticado().getNivel().equals(CodigoNivel.XBRAIN.name())) {
             autenticacaoService.logoutAllUsers();
