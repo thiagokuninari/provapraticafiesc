a22210df0ad03f0baf9e28306ddde6aa99cc2540
Marcelo Thomé <marcelothome@xbrain.com.br>
2018-11-26 09:58:08 -0200

    #11606 Adiciona rotina para excluir todos os token meia noite

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/service/UsuarioTimer.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/service/UsuarioTimer.java
@@ -1,19 +1,49 @@
 package br.com.xbrain.autenticacao.modules.usuario.service;
 
 //import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.xbrain.autenticacao.modules.autenticacao.service.AutenticacaoService;
+import br.com.xbrain.autenticacao.modules.comum.exception.PermissaoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Profile("!test")
+@EnableScheduling
 public class UsuarioTimer {
     
     //@Autowired
     //private UsuarioService service;
 
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
     //private static final String EVERY_DAY_AT_TWO_AM = "0 0 2 * * *";
+
+    private static final String EVERY_DAY_AT_MIDNIGHT = "0 0 0 * * *";
+
+    private static final String TIME_ZONE = "America/Sao_Paulo";
 
     //@Scheduled(cron = EVERY_DAY_AT_TWO_AM)
     public void inativarUsuariosSemAcesso() {
         //service.inativarUsuariosSemAcesso();
+    }
+
+    @Transactional
+    @Async
+    @Scheduled(cron = EVERY_DAY_AT_MIDNIGHT, zone = TIME_ZONE)
+    public void deslogarTodosOsUsuarios() {
+        try {
+            autenticacaoService.logoutAllUsers();
+            return;
+        } catch (Exception e) {
+            throw new PermissaoException();
+        }
     }
 
 }
