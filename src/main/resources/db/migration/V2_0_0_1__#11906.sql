540f3e3ea72fc062f0f757d54c2aaaaf92f9ef36
Renato Alexsander <renato@xbrain.com.br>
2019-01-21 11:23:30 -0200

    Adiciona migration corrigido

--- src/main/resources/db/migration/V2_0_0_1__#11906.sql
+++ src/main/resources/db/migration/V2_0_0_1__#11906.sql
@@ -9,11 +9,11 @@ create table solicitacao_ramal (id integer not null,
                                 quantidade_ramais integer not null,
                                 situacao varchar(255),
                                 telefone_ti varchar(255),
-                                fk_usuario integer, primary key (id))
+                                fk_usuario integer, primary key (id));
 
-alter table solicitacao_ramal add constraint FK_SOLICITACAO_RAMAL_USUARIO foreign key (fk_usuario) references usuario
+alter table solicitacao_ramal add constraint FK_SOLICITACAO_RAMAL_USUARIO foreign key (fk_usuario) references usuario;
 
-create sequence seq_solicitacao_ramal start with 1 increment by 1
+create sequence seq_solicitacao_ramal start with 1 increment by 1;
 
 
 create table solicitacao_ramal_historico (id integer not null,
@@ -21,11 +21,11 @@ create table solicitacao_ramal_historico (id integer not null,
                                           data_cadastro timestamp not null,
                                           situacao varchar(255) not null,
                                           fk_solicitacao_ramal integer,
-                                          fk_usuario integer, primary key (id))
+                                          fk_usuario integer, primary key (id));
 
 
-alter table solicitacao_ramal_historico add constraint FK_SOLICITACAO_RAMAL_HISTORICO_SOLICITACAO_RAMAL foreign key (fk_solicitacao_ramal) references solicitacao_ramal
-alter table solicitacao_ramal_historico add constraint FK_SOLICITACAO_RAMAL_HISTORICO_USUARIO foreign key (fk_usuario) references usuario
+alter table solicitacao_ramal_historico add constraint FK_SOLIC_RM_HIST_SOLIC_RAMAL foreign key (fk_solicitacao_ramal) references solicitacao_ramal;
+alter table solicitacao_ramal_historico add constraint FK_SOLIC_RM_HIST_USUARIO foreign key (fk_usuario) references usuario;
 
-create sequence seq_solicitacao_ramal_historico start with 1 increment by 1
+create sequence seq_solic_rm_hist start with 1 increment by 1;
 
