0e91d8b1e075c9288325ab2b3724bbd45869eecf
Leonardo Barbieri <leonardobarbieri@xbrain.com.br>
2019-02-11 17:26:48 -0200

    #117014 Remove linha em branco

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/dto/UsuarioCsvResponse.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/dto/UsuarioCsvResponse.java
@@ -46,7 +46,6 @@ public class UsuarioCsvResponse {
         this.unidadesNegocios = removeDuplicadosWmConcat(unidadesNegocios);
         this.empresas = removeDuplicadosWmConcat(empresas);
         this.situacao = situacao;
-
     }
 
     @JsonIgnore
