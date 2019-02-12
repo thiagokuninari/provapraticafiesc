02da2a0daea00577d2e04a6c540d22147cea41ea
Renato Alexsander <renato@xbrain.com.br>
2019-02-12 14:26:51 -0200

    #0cabfec4 cherry pick

---
+++ src/main/java/br/com/xbrain/autenticacao/modules/call/service/CallClient.java
@@ -0,0 +1,26 @@
+package br.com.xbrain.autenticacao.modules.call.service;
+
+import br.com.xbrain.autenticacao.config.feign.FeignSkipBadRequestsConfiguration;
+import br.com.xbrain.autenticacao.modules.call.dto.RamalResponse;
+import br.com.xbrain.autenticacao.modules.call.dto.TelefoniaResponse;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "callClient",
+             url = "${app-config.services.call.url}",
+             configuration = FeignSkipBadRequestsConfiguration.class)
+public interface CallClient {
+
+    String API_CONFIGURACAO_TELEFONIA = "api/configuracao-telefonia";
+    String URL_RAMAL = "api/ramal";
+
+    @GetMapping(API_CONFIGURACAO_TELEFONIA + "/obter-nome-telefonia-por-id/{id}")
+    TelefoniaResponse obterNomeTelefoniaPorId(@PathVariable("id") Integer id);
+
+    @GetMapping(URL_RAMAL + "/agente-autorizado/{id}")
+    List<RamalResponse> obterRamaisParaAgenteAutorizado(@PathVariable("id") Integer id);
+
+}
