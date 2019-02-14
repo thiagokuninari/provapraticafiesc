08af37b434be5bc6c616bb39f7d8f069a47594c7
rafaelzeffa <rafael@xbrain.com.br>
2019-02-14 07:52:17 -0200

    Remove migrations antigos

--- src/main/resources/db/migration/V1_0_13_3__#11065.sql
+++
@@ -1,5 +0,0 @@
--- REMOVE PERMISSAO DE INDICACAO DOS TEMPORARIOS  --
-delete from cargo_depart_func where fk_funcionalidade = 3004 and fk_cargo = 82;
-delete from cargo_depart_func where fk_funcionalidade = 3004 and fk_cargo = 83;
-delete from cargo_depart_func where fk_funcionalidade = 3004 and fk_cargo = 84;
-delete from cargo_depart_func where fk_funcionalidade = 3004 and fk_cargo = 85;
