103494757a48bbf797e51d0730b31c1ec18f6f23
Marcelo Thomé <marcelothome@xbrain.com.br>
2019-02-14 16:31:32 -0200

    #11988 Adiciona endpoint pra recuperar todos os subclusters

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/CidadeController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/CidadeController.java
@@ -6,10 +6,7 @@ import br.com.xbrain.autenticacao.modules.usuario.model.Cidade;
 import br.com.xbrain.autenticacao.modules.usuario.repository.CidadeRepository;
 import br.com.xbrain.autenticacao.modules.usuario.service.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
