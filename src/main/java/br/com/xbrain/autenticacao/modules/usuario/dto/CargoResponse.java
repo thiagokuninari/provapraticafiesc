82fe9d5db11753a68d9737e1b2bfe519b74137c4
andrebartieres <cbxy2812>
2019-02-18 09:58:56 -0300

    #11913 - Corrige hierarquia de cargo superior

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/dto/CargoResponse.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/dto/CargoResponse.java
@@ -1,18 +1,16 @@
 package br.com.xbrain.autenticacao.modules.usuario.dto;
 
 import br.com.xbrain.autenticacao.modules.comum.enums.ESituacao;
-import br.com.xbrain.autenticacao.modules.usuario.enums.CodigoCargo;
 import br.com.xbrain.autenticacao.modules.usuario.model.Cargo;
 import br.com.xbrain.autenticacao.modules.usuario.model.Nivel;
 import lombok.Data;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 public class CargoResponse {
@@ -22,9 +20,7 @@ public class CargoResponse {
     private Integer nivel;
     private String codigo;
     private ESituacao situacao;
-    private Integer cargoSuperiorId;
-    private String cargoSuperiorNome;
-    private CodigoCargo cargoSuperiorCodigo;
+    private boolean possuiCargoSuperior;
 
     public CargoResponse() { }
 
@@ -41,15 +37,10 @@ public class CargoResponse {
     public static CargoResponse convertFrom(Cargo cargo) {
         CargoResponse response = new CargoResponse();
         BeanUtils.copyProperties(cargo, response);
-        response.setNivel(!isEmpty(cargo.getNivel()) ? new Nivel(cargo.getNivel().getId()).getId() : null);
-        response.setSituacao(!isEmpty(cargo.getSituacao()) ? cargo.getSituacao() : null);
-        response.setCodigo(!isEmpty(cargo.getCodigo()) ? cargo.getCodigo().name() : null);
-
-        if (!isEmpty(cargo.getCargoSuperior())) {
-            response.setCargoSuperiorId(cargo.getCargoSuperior().getId());
-            response.setCargoSuperiorNome(cargo.getCargoSuperior().getNome());
-            response.setCargoSuperiorCodigo(cargo.getCargoSuperior().getCodigo());
-        }
+        response.setNivel(!ObjectUtils.isEmpty(cargo.getNivel()) ? new Nivel(cargo.getNivel().getId()).getId() : null);
+        response.setSituacao(!ObjectUtils.isEmpty(cargo.getSituacao()) ? cargo.getSituacao() : null);
+        response.setCodigo(!ObjectUtils.isEmpty(cargo.getCodigo()) ? cargo.getCodigo().name() : null);
+        response.setPossuiCargoSuperior(!CollectionUtils.isEmpty(cargo.getSuperiores()));
         return response;
     }
 }
