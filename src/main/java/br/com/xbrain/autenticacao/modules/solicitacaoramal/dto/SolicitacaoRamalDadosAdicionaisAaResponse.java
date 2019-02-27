a4654018eae68ba3993e836ef5be9bd1086534d5
Renato Alexsander <renato@xbrain.com.br>
2019-02-27 12:04:10 -0300

    #12155 - Adiciona retorno do campo agente autorizado razao social no response dos dados adicionais do aa

--- src/main/java/br/com/xbrain/autenticacao/modules/solicitacaoramal/dto/SolicitacaoRamalDadosAdicionaisAaResponse.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/solicitacaoramal/dto/SolicitacaoRamalDadosAdicionaisAaResponse.java
@@ -1,5 +1,6 @@
 package br.com.xbrain.autenticacao.modules.solicitacaoramal.dto;
 
+import br.com.xbrain.autenticacao.modules.parceirosonline.dto.AgenteAutorizadoResponse;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,14 +12,17 @@ public class SolicitacaoRamalDadosAdicionaisAaResponse {
     private String socioPrincipal;
     private long usuariosAtivos;
     private long quantidadeRamais;
+    private String agenteAutorizadoRazaoSocial;
 
     public static SolicitacaoRamalDadosAdicionaisAaResponse convertFrom(String nomeDiscadora, String nomeSocioPrincipal,
-                                                                 long qtdUsuariosAtivos, long qtdRamais) {
+                                                                        long qtdUsuariosAtivos, long qtdRamais,
+                                                                        AgenteAutorizadoResponse agenteAutorizadoResponse) {
         return SolicitacaoRamalDadosAdicionaisAaResponse.builder()
                 .discadora(nomeDiscadora)
                 .socioPrincipal(nomeSocioPrincipal)
                 .usuariosAtivos(qtdUsuariosAtivos)
                 .quantidadeRamais(qtdRamais)
+                .agenteAutorizadoRazaoSocial(agenteAutorizadoResponse.getRazaoSocial())
                 .build();
     }
 }
