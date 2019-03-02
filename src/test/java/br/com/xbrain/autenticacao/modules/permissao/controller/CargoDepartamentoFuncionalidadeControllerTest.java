cc5f69868fa04cf58e12b1db777424f3d040e3d7
Marcelo Thomé <marcelothome@xbrain.com.br>
2019-03-02 16:12:48 -0300

    # Corrige conflitos

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/controller/CargoDepartamentoFuncionalidadeControllerTest.java
@@ -51,7 +51,7 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .header("Authorization", getAccessToken(mvc, ADMIN))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(634)));
+                .andExpect(jsonPath("$", hasSize(635)));
     }
 
     @Test
@@ -62,7 +62,7 @@ public class CargoDepartamentoFuncionalidadeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(10)))
                 .andExpect(jsonPath("$.totalPages", is(64)))
-                .andExpect(jsonPath("$.totalElements", is(634)));
+                .andExpect(jsonPath("$.totalElements", is(635)));
     }
 
     @Test
