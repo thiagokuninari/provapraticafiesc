53e20e0a06061c2da456a4d1e2fd429373b664be
Renato Alexsander <renato@xbrain.com.br>
2019-05-28 16:56:34 -0300

    #12937 - Revert class CodigoAplicacao

--- src/main/java/br/com/xbrain/autenticacao/modules/permissao/enums/CodigoAplicacao.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/permissao/enums/CodigoAplicacao.java
@@ -2,5 +2,5 @@ package br.com.xbrain.autenticacao.modules.permissao.enums;
 
 public enum CodigoAplicacao {
 
-    INT, AUT, POL, VDS, MLG, SML, FCP, VAR, IND, DSH, COG, SPR, EVD;
+    INT, AUT, POL, VDS, MLG, SML, FCP, VAR, IND, DSH, COG, SPR;
 }
