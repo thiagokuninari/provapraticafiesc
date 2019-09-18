e983640f4c2489c22dd820d9ac96601d04ffc5e5
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2019-09-18 14:41:34 -0300

    #13449 - Corrige checkstyle

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioController.java
@@ -17,9 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.security.Principal;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
