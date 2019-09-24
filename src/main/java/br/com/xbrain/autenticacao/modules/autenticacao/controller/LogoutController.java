a15190f8f5d5cd901f4726e5e934b00505cb85f0
Denner Almeida <denner@xbrain.com.br>
2019-09-24 11:37:56 -0300

    Adequa código de acordo com o code-review

--- src/main/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/autenticacao/controller/LogoutController.java
@@ -1,14 +1,11 @@
 package br.com.xbrain.autenticacao.modules.autenticacao.controller;
 
 import br.com.xbrain.autenticacao.modules.autenticacao.service.AutenticacaoService;
-import br.com.xbrain.autenticacao.modules.comum.exception.PermissaoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static br.com.xbrain.autenticacao.modules.usuario.enums.CodigoFuncionalidade.AUT_DESLOGAR_USUARIO;
 
 @RestController
 @RequestMapping(value = "api/logout")
@@ -24,19 +21,11 @@ public class LogoutController {
 
     @GetMapping("{usuarioId}")
     public void logoutPorId(@PathVariable Integer usuarioId) {
-        if (autenticacaoService.getUsuarioAutenticado().hasPermissao(AUT_DESLOGAR_USUARIO)) {
-            autenticacaoService.logout(usuarioId);
-        } else {
-            throw new PermissaoException();
-        }
+        autenticacaoService.logout(usuarioId);
     }
 
     @GetMapping("/todos-usuarios")
     public void logoutAll() {
-        if (autenticacaoService.getUsuarioAutenticado().hasPermissao(AUT_DESLOGAR_USUARIO)) {
-            autenticacaoService.logoutAllUsers();
-        } else {
-            throw new PermissaoException();
-        }
+        autenticacaoService.logoutAllUsers();
     }
 }
