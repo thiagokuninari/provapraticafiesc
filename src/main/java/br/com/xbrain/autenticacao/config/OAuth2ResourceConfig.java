ca77a4ea1cb3df7c580622be56463bba741c0208
Viola Pfening <viola@xbrain.com.br>
2020-02-10 14:11:04 -0300

    #14560 Corrige segundo do code review

--- src/main/java/br/com/xbrain/autenticacao/config/OAuth2ResourceConfig.java
+++ src/main/java/br/com/xbrain/autenticacao/config/OAuth2ResourceConfig.java
@@ -30,7 +30,7 @@ public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
             "/api/usuarios/resetar-senha/**",
             "/api/public/disparar-timer-inativar-usuarios",
             "/api/usuarios/usuario-funil-prospeccao",
-            "/api/usuarios/gerencia/validar-usuario-existente**"
+            "/api/usuarios/gerencia/validar-usuario-existente"
         };
 
         http
