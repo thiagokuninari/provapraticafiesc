1d0f09cb8a45538968246ea8d2f43e7994e24146
Mateus Kobayashi Scaliante <mateuskobayashi@xbrain.com.br>
2020-02-20 13:59:20 -0300

    #14776 Cria migration p/ projeto backoffice e altera enum nivel

--- src/main/java/br/com/xbrain/autenticacao/modules/comum/model/Organizacao.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/comum/model/Organizacao.java
@@ -1,10 +1,12 @@
 package br.com.xbrain.autenticacao.modules.comum.model;
 
+import br.com.xbrain.autenticacao.modules.usuario.model.Nivel;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 @Table(name = "ORGANIZACAO")
@@ -28,6 +30,14 @@ public class Organizacao {
     @NotNull
     @Column(name = "CODIGO", length = 80, nullable = false, unique = true)
     private String codigo;
+
+    @JoinTable(name = "ORGANIZACAO_NIVEL", joinColumns = {
+            @JoinColumn(name = "FK_NIVEL", referencedColumnName = "id",
+                    foreignKey = @ForeignKey(name = "FK_ORG_NIVEL_NIVEL"))}, inverseJoinColumns = {
+            @JoinColumn(name = "FK_ORGANIZACAO", referencedColumnName = "id",
+                    foreignKey = @ForeignKey(name = "FK_ORG_NIVEL_ORGANIZACAO"))})
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Nivel> niveis;
 
     public Organizacao(Integer id) {
         this.id = id;
