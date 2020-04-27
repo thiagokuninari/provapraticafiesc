1202c37637442150672b42b70d5c5ca0764c1c10
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2020-04-27 17:27:17 -0300

    #13006 - Adiciona projeto colaborador-vendas-api

--- src/main/java/br/com/xbrain/autenticacao/config/AuthServerConfig.java
+++ src/main/java/br/com/xbrain/autenticacao/config/AuthServerConfig.java
@@ -100,6 +100,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     private String asteriskUraApiClient;
     @Value("${app-config.oauth-clients.asterisk-ura-api.secret}")
     private String asteriskuraApiSecret;
+    @Value("${app-config.oauth-clients.colaborador-vendas-api.client}")
+    private String colaboradorVendasApiClient;
+    @Value("${app-config.oauth-clients.colaborador-vendas-api.secret}")
+    private String colaboradorVendasApiSecret;
     @Autowired
     private CustomTokenEndpointAuthenticationFilter customTokenEndpointAuthenticationFilter;
     @Autowired
@@ -227,6 +231,12 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
             .secret(asteriskuraApiSecret)
             .authorizedGrantTypes("client_credentials")
             .scopes("asterisk-ura-api")
+            .authorities(ROLE_APPLICATION)
+            .and()
+            .withClient(colaboradorVendasApiClient)
+            .secret(colaboradorVendasApiSecret)
+            .authorizedGrantTypes("client_credentials")
+            .scopes("colaborador-vendas-api")
             .authorities(ROLE_APPLICATION);
     }
 
