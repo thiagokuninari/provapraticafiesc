8137a95150f24693c08ae757f9aea8f375ce23f1
gabriel <gabrielmendes@xbrain.com.br>
2020-05-04 14:19:53 -0300

    #14630 - corrige retorno do metodo

---
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/dto/CidadeUfResponse.java
@@ -0,0 +1,28 @@
+package br.com.xbrain.autenticacao.modules.usuario.dto;
+
+import br.com.xbrain.autenticacao.modules.usuario.model.Cidade;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CidadeUfResponse {
+
+    private Integer cidadeId;
+    private String cidade;
+    private String uf;
+    private Integer ufId;
+
+    public static CidadeUfResponse of(Cidade cidade) {
+        return CidadeUfResponse.builder()
+            .cidadeId(cidade.getId())
+            .cidade(cidade.getNome())
+            .uf(cidade.getNomeUf())
+            .ufId(cidade.getIdUf())
+            .build();
+    }
+}
