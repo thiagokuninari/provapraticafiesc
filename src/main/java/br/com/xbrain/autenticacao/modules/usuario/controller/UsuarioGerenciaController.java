26c257b0ba2627e2cefa8b7d204d88d612831095
Alisson Margiotti <alissonmargiotti@xbrain.com.br>
2020-05-13 16:58:42 -0300

    #14656 Corrige checkstyle

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioGerenciaController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioGerenciaController.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "api/usuarios/gerencia")
