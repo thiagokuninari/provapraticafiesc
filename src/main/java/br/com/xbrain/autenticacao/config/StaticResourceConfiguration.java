471186e88400bd416a08081464578448b9ddc0b4
Denner <denner@xbrain.com.br>
2020-05-26 10:37:48 -0300

    Corrige Checkstyle

--- src/main/java/br/com/xbrain/autenticacao/config/StaticResourceConfiguration.java
+++ src/main/java/br/com/xbrain/autenticacao/config/StaticResourceConfiguration.java
@@ -5,7 +5,6 @@ import br.com.xbrain.autenticacao.modules.logrequest.service.LogRequestIntercept
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
