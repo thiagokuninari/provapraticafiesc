4a7b72e611c67dc012d373658f9d32b81d4ba789
Kao Yu Chun <kaoyuchun@xbrain.com.br>
2020-06-05 18:26:44 -0300

    #15389 Altera teste unitário

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/enums/ECanal.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/enums/ECanal.java
@@ -10,9 +10,10 @@ public enum ECanal {
 
     AGENTE_AUTORIZADO("Agente autorizado", Boolean.TRUE),
     D2D_PROPRIO("D2D Próprio", Boolean.TRUE),
+    ATIVO_PROPRIO("Ativo Local Próprio", Boolean.TRUE),
     ATIVO("Ativo", Boolean.FALSE),
-    ATP("Atp", Boolean.FALSE),
-    ATIVO_PROPRIO("Ativo proprio", Boolean.TRUE);
+    ATP("Atp", Boolean.FALSE);
+
 
     @Getter
     private String descricao;
