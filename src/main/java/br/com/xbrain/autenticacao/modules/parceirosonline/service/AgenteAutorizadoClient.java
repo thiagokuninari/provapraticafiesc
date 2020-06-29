3b41d1f8225736e9cb38d11ab6207bdbb746648d
Gustavo Dias de Oliveira <gustavooliveira@xbrain.com.br>
2020-06-29 10:45:22 -0300

    #15466 Corrige feign

--- src/main/java/br/com/xbrain/autenticacao/modules/parceirosonline/service/AgenteAutorizadoClient.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/parceirosonline/service/AgenteAutorizadoClient.java
@@ -2,7 +2,10 @@ package br.com.xbrain.autenticacao.modules.parceirosonline.service;
 
 import br.com.xbrain.autenticacao.config.feign.FeignSkipBadRequestsConfiguration;
 import br.com.xbrain.autenticacao.modules.comum.dto.EmpresaResponse;
-import br.com.xbrain.autenticacao.modules.parceirosonline.dto.*;
+import br.com.xbrain.autenticacao.modules.parceirosonline.dto.AgenteAutorizadoPermitidoResponse;
+import br.com.xbrain.autenticacao.modules.parceirosonline.dto.AgenteAutorizadoResponse;
+import br.com.xbrain.autenticacao.modules.parceirosonline.dto.UsuarioAgenteAutorizadoAgendamentoResponse;
+import br.com.xbrain.autenticacao.modules.parceirosonline.dto.UsuarioAgenteAutorizadoResponse;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,7 +39,7 @@ public interface AgenteAutorizadoClient {
     boolean isExclusivoPme(@PathVariable("usuarioId") Integer usuarioId);
 
     @GetMapping(API_AGENTE_AUTORIZADO + "/{usuarioId}/estrutura")
-    String getEstrutura(@PathVariable Integer usuarioId);
+    String getEstrutura(@PathVariable("usuarioId") Integer usuarioId);
 
     @GetMapping(API_USUARIO_AGENTE_AUTORIZADO + "/{agenteAutorizadoId}")
     List<UsuarioAgenteAutorizadoResponse> getUsuariosByAaId(
