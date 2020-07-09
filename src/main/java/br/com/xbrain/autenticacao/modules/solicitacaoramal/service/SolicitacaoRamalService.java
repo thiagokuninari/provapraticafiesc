ee9f041f33598e869ae1e980ced7312a802f248d
Kao Yu Chun <kaoyuchun@xbrain.com.br>
2020-07-09 09:36:16 -0300

    15389 - Inserir um campo  no template de e-mail

--- src/main/java/br/com/xbrain/autenticacao/modules/solicitacaoramal/service/SolicitacaoRamalService.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/solicitacaoramal/service/SolicitacaoRamalService.java
@@ -228,6 +228,7 @@ public class SolicitacaoRamalService {
         context.setVariable("dataAtual", DateUtils.parseLocalDateTimeToString(LocalDateTime.now()));
         context.setVariable("codigo", solicitacaoRamal.getId());
         context.setVariable("situacao", solicitacaoRamal.getSituacao());
+        context.setVariable("tipoImplantação", solicitacaoRamal.getTipoImplantacao());
         context.setVariable("melhorDataImplantacao", DateUtils.parseLocalDateToString(
                 solicitacaoRamal.getMelhorDataImplantacao()));
         context.setVariable("melhorHoraImplantacao", solicitacaoRamal.getMelhorHorarioImplantacao());
