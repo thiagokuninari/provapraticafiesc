446d358e1da18ae00ab9149b15cc1dcbe6630009
Lincoln Berlick <lincoln@xbrain.com.br>
2020-12-21 10:57:46 -0300

    #16560 Adiciona construtor equipedto

--- src/main/java/br/com/xbrain/autenticacao/modules/equipevenda/dto/EquipeVendaDto.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/equipevenda/dto/EquipeVendaDto.java
@@ -1,12 +1,16 @@
 package br.com.xbrain.autenticacao.modules.equipevenda.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EquipeVendaDto implements Serializable {
 
     private Integer id;
