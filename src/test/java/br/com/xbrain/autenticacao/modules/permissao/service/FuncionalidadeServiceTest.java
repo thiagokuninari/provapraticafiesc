49581feafdda4db1af3a7888026273cea6765387
Fagner Henrique Santos Onesko <fagneronesko@xbrain.com.br>
2021-02-25 17:25:50 -0300

    #15941 Adiciona role visualizar indicações premium

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/service/FuncionalidadeServiceTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/service/FuncionalidadeServiceTest.java
@@ -73,7 +73,7 @@ public class FuncionalidadeServiceTest {
                 .build();
 
         assertThat(service.getFuncionalidadesPermitidasAoUsuario(usuario))
-                .hasSize(37);
+                .hasSize(38);
     }
 
     private Cargo umCargoMsoConsultor() {
