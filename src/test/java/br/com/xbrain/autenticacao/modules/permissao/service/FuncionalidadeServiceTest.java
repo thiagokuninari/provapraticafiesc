8b1fcfdfd520f1133c08c988e8f5c4fd7d16afcf
arthur-andraadee <arthurandrade@xbrain.com.br>
2021-08-25 14:33:51 -0300

    #17586 Arruma teste

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/service/FuncionalidadeServiceTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/service/FuncionalidadeServiceTest.java
@@ -73,7 +73,7 @@ public class FuncionalidadeServiceTest {
                 .build();
 
         assertThat(service.getFuncionalidadesPermitidasAoUsuario(usuario))
-                .hasSize(43);
+                .hasSize(44);
     }
 
     private Cargo umCargoMsoConsultor() {
