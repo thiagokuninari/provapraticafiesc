f1ce6c6c1fcff5da2a77427e5e3ada7ee2a1aede
Alexandre Kawamura Tanaka <alexandretanaka@xbrain.com.br>
2021-11-11 10:05:45 -0300

    #17097 Remove importações não utilizadas

--- src/test/java/br/com/xbrain/autenticacao/modules/usuarioacesso/service/UsuarioAcessoServiceIT.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/usuarioacesso/service/UsuarioAcessoServiceIT.java
@@ -17,7 +17,6 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
