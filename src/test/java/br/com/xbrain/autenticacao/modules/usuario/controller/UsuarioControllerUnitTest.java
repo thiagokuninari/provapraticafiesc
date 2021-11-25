39b94a2c151561c3d0eb17bdbaf509c07604b6cb
LucasRM <lucasmoraes@xbrain.com.br>
2021-11-25 11:42:42 -0300

    #18201 Apontamentos code review

--- src/test/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioControllerUnitTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/usuario/controller/UsuarioControllerUnitTest.java
@@ -30,6 +30,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
