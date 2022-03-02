25af6bebe4315ee57caeab6b44f6ba0dc8c699b9
Pedro Medeiros <pedromedeiros@xbrain.com.br>
2022-03-02 11:55:11 -0300

    #16246 adiciona id ao codigo departamento enum

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/enums/CodigoDepartamento.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/enums/CodigoDepartamento.java
@@ -8,16 +8,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public enum CodigoDepartamento {
 
-    AGENTE_AUTORIZADO,
+    AGENTE_AUTORIZADO(40),
     COMERCIAL(3),
-    HELP_DESK,
-    ADMINISTRADOR,
-    VAREJO,
-    ATENDIMENTO_JEC,
-    FEEDER,
-    OUVIDORIA,
-    INTEGRACAO,
-    TREINAMENTO;
+    HELP_DESK(51),
+    ADMINISTRADOR(50),
+    VAREJO(54),
+    ATENDIMENTO_JEC(64),
+    ATENDIMENT0_JEC_DOIS(65),
+    FEEDER(68),
+    OUVIDORIA(66),
+    INTEGRACAO(1001),
+    TREINAMENTO(12),
+    TREINAMENTO_DOIS(30);
+
 
     @Getter
     int codigo;
