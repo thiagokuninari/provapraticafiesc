4baec91509d82b14577cd8e26bf4174049239679
henrique-pereira <henriquepereira@xbrain.com.br>
2022-08-04 11:24:53 -0300

    #19076 Ajusta testes referentes a classe CidadeService

--- src/test/java/br/com/xbrain/autenticacao/modules/usuario/service/CidadeServiceTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/usuario/service/CidadeServiceTest.java
@@ -2,7 +2,6 @@ package br.com.xbrain.autenticacao.modules.usuario.service;
 
 import br.com.xbrain.autenticacao.modules.autenticacao.service.AutenticacaoService;
 import br.com.xbrain.autenticacao.modules.comum.exception.ValidacaoException;
-import br.com.xbrain.autenticacao.modules.usuario.dto.CodigoIbgeRegionalResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +13,8 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
@@ -78,33 +75,5 @@ public class CidadeServiceTest {
         assertThat(service.findFirstByUfNomeAndCidadeNome("SP", "SAO PAULO"))
             .extracting("id", "nome")
             .containsExactly(6578, "SAO PAULO");
-    }
-
-    @Test
-    public void getCodigoIbgeRegionalByCidade_deveRetornarListaVazia_quandoInformarListaVaziaDeCidadesId() {
-        assertThat(service.getCodigoIbgeRegionalByCidade(List.of()))
-            .hasSize(0);
-        assertThat(service.getCodigoIbgeRegionalByCidade(List.of()))
-            .isEqualTo(new ArrayList<CodigoIbgeRegionalResponse>());
-    }
-
-    @Test
-    public void getCodigoIbgeRegionalByCidade_deveRetornarListaVazia_quandoInformarListaComCidadeIdNaoExistente() {
-        assertThat(service.getCodigoIbgeRegionalByCidade(List.of(123123, 213213)))
-            .hasSize(0);
-        assertThat(service.getCodigoIbgeRegionalByCidade(List.of()))
-            .isEqualTo(new ArrayList<CodigoIbgeRegionalResponse>());
-    }
-
-    @Test
-    public void getCodigoIbgeRegionalByCidade_deveRetornarListaCodigoIbgeRegionalResponse_quandoEncontrarPorCidadeId() {
-        assertThat(service.getCodigoIbgeRegionalByCidade(List.of(5578, 3426, 4498)))
-            .hasSize(3);
-        assertThat(service.getCodigoIbgeRegionalByCidade(List.of(5578, 3426, 4498)))
-            .extracting("cidadeId", "cidadeNome", "codigoIbge", "regionalId", "regionalNome")
-            .containsExactlyInAnyOrder(
-                tuple(5578, "LONDRINA", "4113700", 3, "SUL"),
-                tuple(3426, "MARINGA", "4115200", 3, "SUL"),
-                tuple(4498, "CHAPECO", "4204202", 3, "SUL"));
     }
 }
