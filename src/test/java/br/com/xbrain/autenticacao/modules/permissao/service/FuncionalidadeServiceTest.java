f0f971dea20575e69b33badd3f18b7e1ba2b39ed
Gabriel Monteiro Motta <gabrielmotta@xbrain.com.br>
2022-12-08 18:02:41 -0300

    #19752 - adiciona funcionalidade para gerenciar mailing layout massivo

--- src/test/java/br/com/xbrain/autenticacao/modules/permissao/service/FuncionalidadeServiceTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/permissao/service/FuncionalidadeServiceTest.java
@@ -75,7 +75,7 @@ public class FuncionalidadeServiceTest {
             .build();
 
         assertThat(service.getFuncionalidadesPermitidasAoUsuario(usuario))
-                .hasSize(43);
+                .hasSize(44);
     }
 
     @Test
