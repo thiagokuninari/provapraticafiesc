72ce86fff40787b6d2e97afb0393b49be175eeb2
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2023-02-27 14:00:04 -0300

    #19715 Resolve TODOs

--- src/main/java/br/com/xbrain/autenticacao/modules/permissao/enums/CodigoAplicacao.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/permissao/enums/CodigoAplicacao.java
@@ -1,8 +1,7 @@
 package br.com.xbrain.autenticacao.modules.permissao.enums;
 
-// TODO Remover alterações
 public enum CodigoAplicacao {
 
     INT, AUT, POL, VDS, MLG, SML, FCP, VAR, IND, DSH, COG, SPR, EVD, CRN, CHM, FPC,
-    BRF, REL, OUV, CTR, CMD, BKO, GRL, TRN, FDR, CHB, PAP, COB, LJF;
+    BRF, REL, OUV, CTR, CMD, BKO, GRL, TRN, FDR, CHB, PAP, COB;
 }
