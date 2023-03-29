d62c3046debefa5b62a1999c2f8d2a2332c14aee
igorbavand <igor.bvn@gmail.com>
2023-03-29 13:34:29 -0300

    #20087 Adiciona client-secret para o bloqueio-ligacao-api

--- src/main/java/br/com/xbrain/autenticacao/config/EScopes.java
+++ src/main/java/br/com/xbrain/autenticacao/config/EScopes.java
@@ -27,7 +27,8 @@ public enum EScopes {
     GERADOR_LEAD("gerador-lead-api"),
     CHATBOT("chatbot-api"),
     SOLICITACAO_PAP("solicitacao-pap"),
-    CLARO_INDICO("claro-indico-api");
+    CLARO_INDICO("claro-indico-api"),
+    BLOQUEIO_LIGACAO_API("bloqueio-ligacao-api");
 
     @Getter
     private String scope;
