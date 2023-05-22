0ae37784364b43e71e109458f78c778b1043c52d
Henrique Pereira <henriquepereira@xbrain.com.br>
2023-05-22 15:19:35 -0300

    20399 Adiciona helpers

--- src/test/java/br/com/xbrain/autenticacao/modules/usuario/helpers/UsuarioHelper.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/usuario/helpers/UsuarioHelper.java
@@ -2,7 +2,6 @@ package br.com.xbrain.autenticacao.modules.usuario.helpers;
 
 import br.com.xbrain.autenticacao.modules.comum.enums.ESituacao;
 import br.com.xbrain.autenticacao.modules.comum.enums.Eboolean;
-import br.com.xbrain.autenticacao.modules.comum.enums.Eboolean;
 import br.com.xbrain.autenticacao.modules.comum.model.Empresa;
 import br.com.xbrain.autenticacao.modules.comum.model.UnidadeNegocio;
 import br.com.xbrain.autenticacao.modules.usuario.dto.UsuarioDto;
@@ -247,6 +246,65 @@ public class UsuarioHelper {
             .hierarquiasId(List.of())
             .recuperarSenhaTentativa(0)
             .tiposFeeder(Set.of())
+            .build();
+    }
+
+    public static Usuario umUsuarioComCidades() {
+        return Usuario.builder()
+            .id(121314)
+            .empresas(List.of())
+            .unidadesNegocios(List.of())
+            .canais(Set.of())
+            .cargo(new Cargo())
+            .departamento(new Departamento())
+            .usuariosHierarquia(Set.of())
+            .cidades(UsuarioCidadeHelper.listaUsuarioCidadesDoParana())
+            .build();
+    }
+
+    public static Usuario umUsuarioComDistritos() {
+        return Usuario.builder()
+            .id(151617)
+            .empresas(List.of())
+            .unidadesNegocios(List.of())
+            .canais(Set.of())
+            .cargo(new Cargo())
+            .departamento(new Departamento())
+            .usuariosHierarquia(Set.of())
+            .cidades(UsuarioCidadeHelper.listaUsuarioCidadeDeDistritosDeLondrina())
+            .build();
+    }
+
+    public static Usuario umUsuarioSemCidades() {
+        return Usuario.builder()
+            .id(181920)
+            .empresas(List.of())
+            .unidadesNegocios(List.of())
+            .canais(Set.of())
+            .cargo(new Cargo())
+            .departamento(new Departamento())
+            .usuariosHierarquia(Set.of())
+            .cidades(Set.of())
+            .build();
+    }
+
+    public static Usuario umUsuarioSiteSupervisor() {
+        return Usuario.builder()
+            .id(1655626)
+            .nome("NOME USUARIO SITE SUPERVISOR")
+            .email("SITESUPERVISOR@XBRAIN.COM.BR")
+            .cargo(CargoHelper.umCargo(10, CodigoCargo.SUPERVISOR_OPERACAO))
+            .situacao(ESituacao.A)
+            .build();
+    }
+
+    public static Usuario umUsuarioSiteCoordenador() {
+        return Usuario.builder()
+            .id(1655607)
+            .nome("NOME USUARIO SITE COORDENADOR")
+            .email("SITECOORDENADOR@XBRAIN.COM.BR")
+            .cargo(CargoHelper.umCargo(4, CodigoCargo.COORDENADOR_OPERACAO))
+            .situacao(ESituacao.A)
             .build();
     }
 }
