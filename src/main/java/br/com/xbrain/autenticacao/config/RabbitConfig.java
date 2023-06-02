722635e91e775d8b5222c914deb9f67258d32265
João Fiorillo <joao.banci@xbrain.com.br>
2023-06-02 16:10:07 -0300

    #19996 adiciona fluxo async

--- src/main/java/br/com/xbrain/autenticacao/config/RabbitConfig.java
+++ src/main/java/br/com/xbrain/autenticacao/config/RabbitConfig.java
@@ -23,12 +23,6 @@ public class RabbitConfig {
     @Value("${app-config.queue.usuario-cadastro-socio-principal-success}")
     private String usuarioCadastroSocioPrincipalSuccessMq;
 
-    @Value("${app-config.queue.usuario-atualizar-socio-principal-success}")
-    private String usuarioAtualizarSocioPrincipalSuccessMq;
-
-    @Value("${app-config.queue.usuario-atualizar-socio-principal-failure}")
-    private String usuarioAtualizarSocioPrincipalFailureMq;
-
     @Value("${app-config.queue.usuario-cadastro-loja-futuro-success}")
     private String usuarioCadastroLojaFuturoSuccessMq;
 
@@ -37,18 +31,6 @@ public class RabbitConfig {
 
     @Value("${app-config.queue.usuario-cadastro-failure}")
     private String usuarioCadastroFailureMq;
-
-    @Value("${app-config.queue.organizacao-empresa-cadastro-success}")
-    private String organizacaoEmpresaCadastroSuccessMq;
-
-    @Value("${app-config.queue.organizacao-empresa-atualizacao-success}")
-    private String organizacaoEmpresaAtualizacaoSuccessMq;
-
-    @Value("${app-config.queue.organizacao-empresa-inativar-situacao-success}")
-    private String organizacaoEmpresaInativarSituacaoSuccessMq;
-
-    @Value("${app-config.queue.organizacao-empresa-ativar-situacao-success}")
-    private String organizacaoEmpresaAtivarSituacaoSuccessMq;
 
     @Value("${app-config.queue.usuario-atualizacao}")
     private String usuarioAtualizacaoMq;
@@ -97,9 +79,6 @@ public class RabbitConfig {
 
     @Value("${app-config.queue.inativar-colaborador-pol}")
     private String inativarColaboradorPolMq;
-
-    @Value("${app-config.queue.inativar-colaborador-pol-failure}")
-    private String inativarColaboradorPolFailureMq;
 
     @Value("${app-config.queue.usuario-logout}")
     private String usuarioLogoutMq;
@@ -158,12 +137,6 @@ public class RabbitConfig {
     @Value("${app-config.queue.permissao-agente-autorizado-equipe-tecnica-failure}")
     private String permissaoAgenteAutorizadoEquipeTecnicaFailureMq;
 
-    @Value("${app-config.queue.atualizar-permissao-tecnico-indicador}")
-    private String atualizarPermissaoTecnicoIndicadorMq;
-
-    @Value("${app-config.queue.atualizar-permissao-tecnico-indicador-failure}")
-    private String atualizarPermissaoTecnicoIndicadorFailureMq;
-
     @Bean
     public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
         return new Jackson2JsonMessageConverter(objectMapper);
@@ -213,19 +186,6 @@ public class RabbitConfig {
     }
 
     @Bean
-    Queue usuarioAtualizarSocioPrincipalSuccessMq() {
-        return QueueBuilder.durable(usuarioAtualizarSocioPrincipalSuccessMq)
-            .withArgument(DEAD_LETTER_EXCHANGE, "")
-            .withArgument(DEAD_LETTER_ROUTING_KEY, usuarioAtualizarSocioPrincipalFailureMq)
-            .build();
-    }
-
-    @Bean
-    Queue usuarioAtualizarSocioPrincipalFailureMq() {
-        return QueueBuilder.durable(usuarioAtualizarSocioPrincipalFailureMq).build();
-    }
-
-    @Bean
     Queue usuarioCadastroLojaFuturoSuccessMq() {
         return new Queue(usuarioCadastroLojaFuturoSuccessMq, false);
     }
@@ -242,36 +202,12 @@ public class RabbitConfig {
 
     @Bean
     Queue inativarColaboradorPolMq() {
-        return QueueBuilder
-            .durable(inativarColaboradorPolMq)
-            .withArgument(DEAD_LETTER_EXCHANGE, "")
-            .withArgument(DEAD_LETTER_ROUTING_KEY, inativarColaboradorPolFailureMq)
-            .build();
+        return new Queue(inativarColaboradorPolMq, false);
     }
 
     @Bean
     Queue usuarioCadastroSuccessMq() {
         return new Queue(usuarioCadastroSuccessMq, false);
-    }
-
-    @Bean
-    Queue organizacaoEmpresaCadastroSuccessMq() {
-        return new Queue(organizacaoEmpresaCadastroSuccessMq, false);
-    }
-
-    @Bean
-    Queue organizacaoEmpresaAtualizacaoSuccessMq() {
-        return new Queue(organizacaoEmpresaAtualizacaoSuccessMq, false);
-    }
-
-    @Bean
-    Queue organizacaoEmpresaInativarSituacaoSuccessMq() {
-        return new Queue(organizacaoEmpresaInativarSituacaoSuccessMq, false);
-    }
-
-    @Bean
-    Queue organizacaoEmpresaAtivarSituacaoSuccessMq() {
-        return new Queue(organizacaoEmpresaAtivarSituacaoSuccessMq, false);
     }
 
     @Bean
@@ -431,20 +367,6 @@ public class RabbitConfig {
     }
 
     @Bean
-    Queue atualizarPermissaoTecnicoIndicadorMq() {
-        return QueueBuilder
-            .durable(atualizarPermissaoTecnicoIndicadorMq)
-            .withArgument(DEAD_LETTER_EXCHANGE, "")
-            .withArgument(DEAD_LETTER_ROUTING_KEY, atualizarPermissaoTecnicoIndicadorFailureMq)
-            .build();
-    }
-
-    @Bean
-    Queue atualizarPermissaoTecnicoIndicadorFailureMq() {
-        return QueueBuilder.durable(atualizarPermissaoTecnicoIndicadorFailureMq).build();
-    }
-
-    @Bean
     public Binding usuarioCadastroBinding(TopicExchange exchange) {
         return BindingBuilder.bind(usuarioCadastroMq()).to(exchange).with(usuarioCadastroMq);
     }
@@ -491,30 +413,6 @@ public class RabbitConfig {
     @Bean
     public Binding usuarioCadastroSuccessFailureBinding(TopicExchange exchange) {
         return BindingBuilder.bind(usuarioCadastroFailureMq()).to(exchange).with(usuarioCadastroFailureMq);
-    }
-
-    @Bean
-    public Binding organizacaoEmpresaCadastroSuccessBinding(TopicExchange exchange) {
-        return BindingBuilder.bind(organizacaoEmpresaCadastroSuccessMq()).to(exchange)
-            .with(organizacaoEmpresaCadastroSuccessMq);
-    }
-
-    @Bean
-    public Binding organizacaoEmpresaAtualizacaoSuccessBinding(TopicExchange exchange) {
-        return BindingBuilder.bind(organizacaoEmpresaAtualizacaoSuccessMq()).to(exchange)
-            .with(organizacaoEmpresaAtualizacaoSuccessMq);
-    }
-
-    @Bean
-    public Binding organizacaoEmpresaInativarSituacaoSuccessBinding(TopicExchange exchange) {
-        return BindingBuilder.bind(organizacaoEmpresaInativarSituacaoSuccessMq()).to(exchange)
-            .with(organizacaoEmpresaInativarSituacaoSuccessMq);
-    }
-
-    @Bean
-    public Binding organizacaoEmpresaAtivarSituacaoSuccessBinding(TopicExchange exchange) {
-        return BindingBuilder.bind(organizacaoEmpresaAtivarSituacaoSuccessMq()).to(exchange)
-            .with(organizacaoEmpresaAtivarSituacaoSuccessMq);
     }
 
     @Bean
@@ -661,19 +559,5 @@ public class RabbitConfig {
         return BindingBuilder.bind(usuarioInativacaoPorAaMq())
             .to(exchange)
             .with(usuarioInativacaoPorAaMq);
-    }
-
-    @Bean
-    public Binding atualizarPermissaoTecnicoIndicadorMqBinding(TopicExchange exchange) {
-        return BindingBuilder.bind(atualizarPermissaoTecnicoIndicadorMq())
-            .to(exchange)
-            .with(atualizarPermissaoTecnicoIndicadorMq);
-    }
-
-    @Bean
-    public Binding atualizarPermissaoTecnicoIndicadorFailureMqBinding(TopicExchange exchange) {
-        return BindingBuilder.bind(atualizarPermissaoTecnicoIndicadorFailureMq())
-            .to(exchange)
-            .with(atualizarPermissaoTecnicoIndicadorFailureMq);
     }
 }
