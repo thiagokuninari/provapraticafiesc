eea5ed0bda2d897d0d4dffd2c18b525a1efa56bd
João Fiorillo <joao.banci@xbrain.com.br>
2023-06-21 17:31:57 -0300

    corrige discussions

--- src/main/java/br/com/xbrain/autenticacao/modules/feriado/importacaoautomatica/repository/ImportacaoAutomaticaFeriadoRepository.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/feriado/importacaoautomatica/repository/ImportacaoAutomaticaFeriadoRepository.java
@@ -1,15 +1,10 @@
 package br.com.xbrain.autenticacao.modules.feriado.importacaoautomatica.repository;
 
 import br.com.xbrain.autenticacao.modules.feriado.importacaoautomatica.model.ImportacaoFeriado;
-import com.querydsl.core.types.Predicate;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ImportacaoAutomaticaFeriadoRepository extends PagingAndSortingRepository<ImportacaoFeriado, Integer>,
     QueryDslPredicateExecutor<ImportacaoFeriado> {
-
-    Page<ImportacaoFeriado> findAll(Predicate predicate, Pageable pageable) ;
 
 }
