2246ec685bda0f4a8766ea55e4435fe14bd75deb
Isabelly Santos <maria.santos@xbrain.com.br>
2023-07-04 14:34:23 -0300

    #20366 - refatora teste de ImportacaoUsuarioController

--- src/test/java/br/com/xbrain/autenticacao/modules/importacaousuario/controller/ImportacaoUsuarioControllerTest.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/importacaousuario/controller/ImportacaoUsuarioControllerTest.java
@@ -1,106 +1,101 @@
 package br.com.xbrain.autenticacao.modules.importacaousuario.controller;
 
-import helpers.Usuarios;
+import br.com.xbrain.autenticacao.config.OAuth2ResourceConfig;
+import br.com.xbrain.autenticacao.modules.equipevenda.service.EquipeVendaD2dService;
+import br.com.xbrain.autenticacao.modules.importacaousuario.service.ImportacaoUsuarioService;
+import lombok.SneakyThrows;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import static br.com.xbrain.autenticacao.modules.importacaousuario.util.FileUtil.getFile;
-import static helpers.TestsHelper.getAccessToken;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional(propagation = Propagation.REQUIRES_NEW)
-@Sql(scripts = {"classpath:/tests_database.sql"})
+@WebMvcTest(controllers = ImportacaoUsuarioController.class)
+@Import(OAuth2ResourceConfig.class)
+@WithMockUser
 public class ImportacaoUsuarioControllerTest {
+
     @Autowired
     private MockMvc mvc;
+    @MockBean
+    private ImportacaoUsuarioService service;
+    @MockBean
+    private EquipeVendaD2dService equipeVendaD2dService;
+    @MockBean
+    private TokenStore tokenStore;
 
     @Test
-    public void deveRetornarComOk() throws Exception {
+    @SneakyThrows
+    @WithAnonymousUser
+    public void uploadUsuario_deveRetornarUnauthorized_quandoNaoPassarToken() {
+        mvc.perform(MockMvcRequestBuilders
+                .fileUpload("/api/importacao-usuarios"))
+            .andExpect(status().isUnauthorized());
+
+        verify(service, never()).salvarUsuarioFile(any(), any());
+    }
+
+    @Test
+    @SneakyThrows
+    public void uploadUsuario_deveRetornarBadRequest_quandoNaoInformarCampoUsuarioImportacaoJson() {
 
         mvc.perform(MockMvcRequestBuilders
                 .fileUpload("/api/importacao-usuarios")
                 .file(new MockMultipartFile("file", "planilha.xlsx",
-                        "application/vnd.ms-excel", getFile("arquivo_usuario/planilha.xlsx")))
-                .param("usuarioImportacaoJson",
-                        "{\"file\":[{\"preview\":"
-                                + "\"blob:http://localhost:3100/5fa6d20c-8b61-4500-a0e9-5c9184e2c36d\"}],"
-                                + "\"senhaPadrao\":true}")
-                .header("Authorization", getAccessToken(mvc, Usuarios.ADMIN))
+                    "application/vnd.ms-excel", getFile("arquivo_usuario/planilha.xlsx")))
                 .accept(MediaType.ALL_VALUE))
-                .andExpect(status().isOk());
+            .andExpect(status().isBadRequest());
+
+        verify(service, never()).salvarUsuarioFile(any(), any());
     }
 
     @Test
-    public void deveRetornarQuantidadeDeItensIndiferenteSePossuiLinhasEmBrancoEntreItens() throws Exception {
+    @SneakyThrows
+    public void uploadUsuario_deveRetornarBadRequest_quandoNaoInformarCampoFile() {
+
+        mvc.perform(MockMvcRequestBuilders
+                .fileUpload("/api/importacao-usuarios")
+                .param("usuarioImportacaoJson",
+                    "{\"file\":[{\"preview\":"
+                        + "\"blob:http://localhost:3100/5fa6d20c-8b61-4500-a0e9-5c9184e2c36d\"}],"
+                        + "\"senhaPadrao\":true}")
+                .accept(MediaType.ALL_VALUE))
+            .andExpect(status().isBadRequest());
+
+        verify(service, never()).salvarUsuarioFile(any(), any());
+    }
+
+    @Test
+    @SneakyThrows
+    public void uploadUsuario_deveRetornarOk_quandoDadosValidos() {
 
         mvc.perform(MockMvcRequestBuilders
                 .fileUpload("/api/importacao-usuarios")
                 .file(new MockMultipartFile("file", "planilha.xlsx",
-                        "application/vnd.ms-excel", getFile("arquivo_usuario/planilha.xlsx")))
+                    "application/vnd.ms-excel", getFile("arquivo_usuario/planilha.xlsx")))
                 .param("usuarioImportacaoJson",
-                        "{\"file\":[{\"preview\":"
-                                + "\"blob:http://localhost:3100/5fa6d20c-8b61-4500-a0e9-5c9184e2c36d\"}],"
-                                + "\"senhaPadrao\":true}")
-                .header("Authorization", getAccessToken(mvc, Usuarios.HELP_DESK))
+                    "{\"file\":[{\"preview\":"
+                        + "\"blob:http://localhost:3100/5fa6d20c-8b61-4500-a0e9-5c9184e2c36d\"}],"
+                        + "\"senhaPadrao\":true}")
                 .accept(MediaType.ALL_VALUE))
-                .andExpect(jsonPath("$", hasSize(15)))
-                .andExpect(status().isOk());
-    }
+            .andExpect(status().isOk());
 
-    @Test
-    public void deveValidarAQuantidadeDeColunasDaPlanilha() throws Exception {
-
-        mvc.perform(MockMvcRequestBuilders
-                .fileUpload("/api/importacao-usuarios")
-                .file(new MockMultipartFile("file", "planilha_sem_fone.xlsx",
-                        "application/vnd.ms-excel", getFile("arquivo_usuario/planilha_sem_fone.xlsx")))
-                .param("usuarioImportacaoJson",
-                        "{\"file\":[{\"preview\":"
-                                + "\"blob:http://localhost:3100/5fa6d20c-8b61-4500-a0e9-5c9184e2c36d\"}],"
-                                + "\"senhaPadrao\":true}")
-                .header("Authorization", getAccessToken(mvc, Usuarios.HELP_DESK))
-                .accept(MediaType.ALL_VALUE))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[*].message",
-                        containsInAnyOrder("Erro. Arquivo Inválido.")));
-    }
-
-    @Test
-    public void deveRetornarErroSeArquivoXlsSejaInvalido() throws Exception {
-
-        mvc.perform(MockMvcRequestBuilders
-                .fileUpload("/api/importacao-usuarios")
-                .file(new MockMultipartFile("file", "planilha_invertida.xlsx",
-                        "application/vnd.ms-excel", getFile("arquivo_usuario/planilha_invertida.xlsx")))
-                .param("usuarioImportacaoJson",
-                        "{\"file\":[{\"preview\":"
-                                + "\"blob:http://localhost:3100/5fa6d20c-8b61-4500-a0e9-5c9184e2c36d\"}],"
-                                + "\"senhaPadrao\":true}")
-                .header("Authorization", getAccessToken(mvc, Usuarios.HELP_DESK))
-                .accept(MediaType.ALL_VALUE))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[*].message",
-                        containsInAnyOrder("Erro. Arquivo Inválido.")));
+        verify(service).salvarUsuarioFile(any(), any());
     }
 }
