ca3392e2315924e59060ff09963c7f407170925d
Isabelly Santos <maria.santos@xbrain.com.br>
2023-07-04 14:29:14 -0300

    #20366 - refatora teste de ModalidadeEmpresaController

--- src/main/java/br/com/xbrain/autenticacao/modules/organizacaoempresa/controller/ModalidadeEmpresaController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/organizacaoempresa/controller/ModalidadeEmpresaController.java
@@ -2,7 +2,7 @@ package br.com.xbrain.autenticacao.modules.organizacaoempresa.controller;
 
 import br.com.xbrain.autenticacao.modules.comum.dto.SelectResponse;
 import br.com.xbrain.autenticacao.modules.organizacaoempresa.service.ModalidadeEmpresaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/modalidade-empresa")
 public class ModalidadeEmpresaController {
 
-    @Autowired
-    private ModalidadeEmpresaService service;
+    private final ModalidadeEmpresaService service;
 
     @GetMapping
     public List<SelectResponse> buscarModalidadeEmpresa() {
