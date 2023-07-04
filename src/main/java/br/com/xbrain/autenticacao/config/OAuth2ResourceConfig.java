779620434db1e7c601bceb76a9a5e4b5b42f210f
lucasmussi27 <lucas.mussi27@outlook.com>
2023-07-04 15:32:44 -0300

    #20633 Remove configurações para testes de controllers

--- src/main/java/br/com/xbrain/autenticacao/config/OAuth2ResourceConfig.java
+++ src/main/java/br/com/xbrain/autenticacao/config/OAuth2ResourceConfig.java
@@ -3,7 +3,6 @@ package br.com.xbrain.autenticacao.config;
 import br.com.xbrain.autenticacao.modules.usuario.enums.CodigoFuncionalidade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,8 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
@@ -21,7 +18,7 @@ public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private TokenStore tokenStore;
     @Autowired
-    private Environment environment;
+    private CorsConfigFilter corsConfigFilter;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -47,7 +44,7 @@ public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
         };
 
         http
-            .addFilterBefore(new CorsConfigFilter(), ChannelProcessingFilter.class)
+            .addFilterBefore(corsConfigFilter, ChannelProcessingFilter.class)
             .requestMatchers()
             .antMatchers("/**")
             .and()
@@ -97,13 +94,6 @@ public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
-        if (isActiveProfileTest()) {
-            resources.stateless(false);
-        }
         resources.tokenStore(tokenStore);
-    }
-
-    private boolean isActiveProfileTest() {
-        return Arrays.stream(environment.getActiveProfiles()).anyMatch(env -> env.equalsIgnoreCase("test"));
     }
 }
