dd77f454db079aaa88334ac62dff45acb07f3bbf
felipesantos <felipesantos@xbrain.com.br>
2023-08-16 18:19:49 -0300

    #20690 Corrige typo na migration

--- src/main/resources/db/migration/V4_0_1_1692210545117__#20690.sql
+++ src/main/resources/db/migration/V4_0_1_1692210545117__#20690.sql
@@ -1,15 +1,15 @@
--- Inserir funcionalidade para Inserir planilha para direcionamento Inside Sales Pme por CEP
+-- Inserir funcionalidade Importar planilha direcionamento Inside Sales Pme por CEP
 INSERT INTO FUNCIONALIDADE (ID, NOME, ROLE, FK_APLICACAO)
 VALUES (3073, 'Importar Planilha Direcionamento Inside Sales Pme Por Cep', 'CTR_3073', 20);
 
--- Inserir funcionalidade Inserir planilha para direcionamento Inside Sales Pme por CEP para: Cargo ADMINSTRADOR / Departamento ADMINISTRADOR
+-- Inserir funcionalidade Importar planilha direcionamento Inside Sales Pme por CEP para: Cargo ADMINISTRADOR / Departamento ADMINISTRADOR
 INSERT INTO CARGO_DEPART_FUNC (ID, FK_CARGO, FK_DEPARTAMENTO, FK_FUNCIONALIDADE)
 VALUES (SEQ_CARGO_DEPART_FUNC.nextval, 50, 50, 3073);
 
--- Inserir funcionalidade para consultar direcionamento Inside Sales Pme por CEP
+-- Inserir funcionalidade Consultar direcionamento Inside Sales Pme por CEP
 INSERT INTO FUNCIONALIDADE (ID, NOME, ROLE, FK_APLICACAO)
 VALUES (3074, 'Consultar Direcionamento Inside Sales Pme Por Cep', 'CTR_3074', 20);
 
--- Inserir funcionalidade consultar direcionamento Inside Sales Pme por CEP para: Cargo ADMINSTRADOR / Departamento ADMINISTRADOR
+-- Inserir funcionalidade Consultar direcionamento Inside Sales Pme por CEP para: Cargo ADMINISTRADOR / Departamento ADMINISTRADOR
 INSERT INTO CARGO_DEPART_FUNC (ID, FK_CARGO, FK_DEPARTAMENTO, FK_FUNCIONALIDADE)
 VALUES (SEQ_CARGO_DEPART_FUNC.nextval, 50, 50, 3074);
