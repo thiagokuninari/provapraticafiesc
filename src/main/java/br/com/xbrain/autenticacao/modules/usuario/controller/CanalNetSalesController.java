f29c510bcf804420956bb78642f4f21d8536ab3d
Matheus da Cruz do Nascimento <matheus.nascimento@xbrain.com.br>
2023-11-29 08:53:29 -0300

    #21349-Remove anotacao nao usada

--- src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/CanalNetSalesController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuario/controller/CanalNetSalesController.java
@@ -2,7 +2,6 @@ package br.com.xbrain.autenticacao.modules.usuario.controller;
 
 import br.com.xbrain.autenticacao.modules.comum.dto.SelectResponse;
 import br.com.xbrain.autenticacao.modules.usuario.enums.CanalNetSales;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/usuario-canal-netsales")
 public class CanalNetSalesController {
