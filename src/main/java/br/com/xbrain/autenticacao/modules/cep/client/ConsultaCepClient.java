0d49082c5adf28f2a18c80990203a8087dc70c42
Fiorillo <joao.banci@xbrain.com.br>
2023-11-30 17:09:14 -0300

    20943 adiciona configuration de client

--- src/main/java/br/com/xbrain/autenticacao/modules/cep/client/ConsultaCepClient.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/cep/client/ConsultaCepClient.java
@@ -1,14 +1,11 @@
 package br.com.xbrain.autenticacao.modules.cep.client;
 
-import br.com.xbrain.autenticacao.config.feign.FeignSkipBadRequestsConfiguration;
 import br.com.xbrain.autenticacao.modules.cep.dto.ConsultaCepResponse;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "consultaCepClient",
-    url = "${app-config.services.localizacao.url}",
-    configuration = FeignSkipBadRequestsConfiguration.class)
+@FeignClient(name = "consultaCepClient", url = "${app-config.services.localizacao.url}")
 public interface ConsultaCepClient {
 
     String LOCALIZACAO_ENDPOINT = "api/consulta-cep";
