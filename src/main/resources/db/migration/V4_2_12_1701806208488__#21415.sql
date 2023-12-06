46e126656d4d210b5489c2cf2b6106aa0e7c51fb
Portella <portella.jota@gmail.com>
2023-12-06 16:29:14 -0300

    #21415 - Adiciona nivel briefing e corrige indentação

--- src/main/resources/db/migration/V4_2_12_1701806208488__#21415.sql
+++ src/main/resources/db/migration/V4_2_12_1701806208488__#21415.sql
@@ -5,16 +5,19 @@ BEGIN
 
     IF QTD = 0 THEN
 
+        -- Inserir nivel Briefing para Solicitação de Projetos
+        INSERT INTO NIVEL(ID, CODIGO, EXIBIR_CAD_USUARIO, NOME, SITUACAO) VALUES(20, 'BRIEFING', 'V', 'Briefing', 'A');
+
         -- Inserir funcionalidade para Cadastrar Solicitação de Projetos
         INSERT INTO FUNCIONALIDADE (ID, NOME, ROLE, FK_APLICACAO) VALUES (10011, 'Cadastrar Solicitação de Projetos', 'BRF_10011', 19);
         -- Inserir funcionalidade para Tratar Solicitação de Projetos
         INSERT INTO FUNCIONALIDADE (ID, NOME, ROLE, FK_APLICACAO) VALUES (10013, 'Tratar Solicitação de Projetos', 'BRF_10013', 19);
 
         -- Inserir Cargos para Solicitação de Projetos
-        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (505, 'APROVADOR_PROJETOS_1', 'Aprovador de Projetos 1', 'A', 2);
-        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (506, 'APROVADOR_PROJETOS_2', 'Aprovador de Projetos 2', 'A', 2);
-        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (507, 'APROVADOR_PROJETOS_3', 'Aprovador de Projetos 3', 'A', 2);
-        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (508, 'APROVADOR_PROJETOS_4', 'Aprovador de Projetos 4', 'A', 2);
+        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (505, 'APROVADOR_PROJETOS_1', 'Aprovador de Projetos 1', 'A', 20);
+        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (506, 'APROVADOR_PROJETOS_2', 'Aprovador de Projetos 2', 'A', 20);
+        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (507, 'APROVADOR_PROJETOS_3', 'Aprovador de Projetos 3', 'A', 20);
+        INSERT INTO CARGO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (508, 'APROVADOR_PROJETOS_4', 'Aprovador de Projetos 4', 'A', 20);
 
         -- Inserir Departamento Briefing para Solicitação de Projetos
         INSERT INTO DEPARTAMENTO (ID, CODIGO, NOME, SITUACAO, FK_NIVEL) VALUES (72, 'BRIEFING', 'Briefing', 'A', 2);
