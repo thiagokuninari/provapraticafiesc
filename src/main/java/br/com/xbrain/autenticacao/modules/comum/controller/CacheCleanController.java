6ddcacd495bc9fb83fa55587e79a11ebec82d8b0
João Lima <joao.lima@xbrain.com.br>
2024-02-16 14:06:38 -0300

    #21615 - Adiciona CacheCleanController

--- src/main/java/br/com/xbrain/autenticacao/modules/comum/controller/CacheCleanController.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/comum/controller/CacheCleanController.java
@@ -1,21 +1,22 @@
-package br.com.xbrain.autenticacao.modules.agenteautorizadonovo.controller;
+package br.com.xbrain.autenticacao.modules.comum.controller;
 
 import br.com.xbrain.autenticacao.modules.agenteautorizadonovo.service.AgenteAutorizadoNovoService;
 import br.com.xbrain.autenticacao.modules.autenticacao.service.AutenticacaoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/agentes-autorizados")
-public class AgenteAutorizadoController {
+@RequestMapping("api/cache-clean")
+public class CacheCleanController {
 
     private final AgenteAutorizadoNovoService service;
     private final AutenticacaoService autenticacaoService;
 
-    @PostMapping("cache/estrutura/limpar")
+    @DeleteMapping("agente-autorizado/estrutura")
     public void limparCachesEstruturas() {
         autenticacaoService.getUsuarioAutenticado().validarAdministrador();
         service.flushCacheEstruturasAas();
