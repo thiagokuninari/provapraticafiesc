0b4111e19a4a2a5c1186f8dbe90278e312c1725e
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2024-04-04 16:32:35 -0300

    #21863 - Remove import nao utilizado

--- src/main/java/br/com/xbrain/autenticacao/modules/usuarioacesso/service/UsuarioAcessoService.java
+++ src/main/java/br/com/xbrain/autenticacao/modules/usuarioacesso/service/UsuarioAcessoService.java
@@ -34,7 +34,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
