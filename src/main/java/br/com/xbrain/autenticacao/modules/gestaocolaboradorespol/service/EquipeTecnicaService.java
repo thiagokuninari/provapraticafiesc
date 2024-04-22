5a1e50cd1166c94eb959ff8a745dd72b8d67e0dd
Thiago Gloor <thiago.gloor@xbrain.com.br>
2024-04-22 09:53:27 -0300

    #21727 adiciona equipes tecnicas supervisionadas

---
+++ src/main/java/br/com/xbrain/autenticacao/modules/gestaocolaboradorespol/service/EquipeTecnicaService.java
@@ -0,0 +1,27 @@
+package br.com.xbrain.autenticacao.modules.gestaocolaboradorespol.service;
+
+import br.com.xbrain.autenticacao.modules.gestaocolaboradorespol.client.EquipeTecnicaClient;
+import br.com.xbrain.autenticacao.modules.gestaocolaboradorespol.dto.EquipeTecnicaSupervisionadasResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EquipeTecnicaService {
+
+    private final EquipeTecnicaClient client;
+
+    public List<EquipeTecnicaSupervisionadasResponse> getEquipesPorSupervisor(Integer supervisorId) {
+        try {
+            return client.getEquipesTecnicasPorSupervisor(supervisorId);
+        } catch (Exception ex) {
+            log.warn("Erro ao obter as equipes Tecnicas do supervisor", ex);
+            return Collections.emptyList();
+        }
+    }
+}
