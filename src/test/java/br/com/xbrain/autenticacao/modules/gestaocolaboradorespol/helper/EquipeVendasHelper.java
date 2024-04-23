48dea8e3dd8cf01673db75b0f59a87e09f0a5c55
Thiago Gloor <thiago.gloor@xbrain.com.br>
2024-04-23 18:05:53 -0300

    #21727 cria testes e volta alteracao de application

--- src/test/java/br/com/xbrain/autenticacao/modules/gestaocolaboradorespol/helper/EquipeVendasHelper.java
+++ src/test/java/br/com/xbrain/autenticacao/modules/gestaocolaboradorespol/helper/EquipeVendasHelper.java
@@ -1,4 +1,25 @@
 package br.com.xbrain.autenticacao.modules.gestaocolaboradorespol.helper;
 
-public class EquipeTecnicaHelper {
+import br.com.xbrain.autenticacao.modules.equipevenda.dto.EquipeVendaDto;
+import br.com.xbrain.autenticacao.modules.gestaocolaboradorespol.dto.EquipeVendasSupervisionadasResponse;
+
+public class EquipeVendasHelper {
+
+    public static EquipeVendasSupervisionadasResponse umaEquipeVendasSupervisionadasResponse() {
+        return EquipeVendasSupervisionadasResponse
+            .builder()
+            .id(1)
+            .canalVenda("D2D")
+            .descricao("Equipe Teste")
+            .build();
+    }
+
+    public static EquipeVendaDto umaEquipeVendaDto() {
+        return EquipeVendaDto
+            .builder()
+            .id(1)
+            .canalVenda("TELEVENDAS")
+            .descricao("Equipe Teste 2")
+            .build();
+    }
 }
